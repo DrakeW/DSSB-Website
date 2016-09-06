f4156e0e6911b0dbc90638ae9a09c07695eea3ba
DrakeW <junyuw@berkeley.edu>
2016-09-05 22:01:37 -0700

    added folding to project page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,6 @@
 /* Write here your custom javascript codes */
+
+/* projects page expansion */
+$('#sp-16-projects-headline').click(function(event) {
+	$('#sp-16-projects').css('display', 'block');
+});
